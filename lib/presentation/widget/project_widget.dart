93db3c2988695968220a2e78d18324167237c875
Lorenzo <djonline96@gmail.com>
2021-03-25 17:42:32 +0100

    Partially fix project_widget.dart

--- lib/presentation/widget/project_widget.dart
+++ lib/presentation/widget/project_widget.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio/core/column_extension.dart';
 import 'package:portfolio/domain/project.dart';
 import 'package:portfolio/presentation/app_theme.dart';
-import 'package:portfolio/presentation/widget/link_widget.dart';
 import 'package:portfolio/presentation/widget/project_link_tag.dart';
 import 'package:portfolio/presentation/widget/responsive.dart';
 
@@ -21,37 +19,6 @@ class ProjectWidget extends StatelessWidget {
       small: ProjectContentSmall(project: project),
       large: ProjectContentLarge(project: project),
     );
-  }
-
-  Widget _buildLinksAnsInfo() {
-    return Row(mainAxisSize: MainAxisSize.min, children: [
-      if (project.links != null)
-        RowBuilder(
-          itemCount: project.links!.length,
-          builder: (_, index) => LinkWidget(link: project.links![index]),
-        ),
-      if (project.tags != null)
-        RowBuilder(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          itemCount: project.tags!.length,
-          builder: (_, index) => Text(project.tags![index]),
-          separator: 16.0,
-        ),
-    ]);
-  }
-
-  Widget _buildImages() {
-    if (project.images != null)
-      return ListView.builder(
-        itemCount: project.images!.length,
-        itemBuilder: (_, index) => Placeholder(
-          fallbackHeight: 300.0,
-          fallbackWidth: 150.0,
-        ),
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-      );
-    return SizedBox.shrink();
   }
 }
 
@@ -90,10 +57,11 @@ class ProjectContentSmall extends StatelessWidget {
             child: RowBuilder(
               mainAxisSize: MainAxisSize.min,
               itemCount: project.images!.length,
+              // TODO: Build an Image instead of a container
               builder: (context, index) => Container(
                 color: Colors.red,
-                width: 200.0,
-                height: 450.0,
+                width: 150.0,
+                height: 300.0,
               ),
               separator: 16.0,
             ),
@@ -113,20 +81,24 @@ class ProjectContentLarge extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) => Stack(
+    return Container(
+      width: double.maxFinite,
+      color: Colors.blue,
+      child: Stack(
         children: [
-          Align(
-            alignment: Alignment.topRight,
-            child: RowBuilder(
-              mainAxisSize: MainAxisSize.min,
-              itemCount: project.images?.length ?? 0,
-              builder: (context, index) => Container(
-                color: Colors.red,
-                width: 200.0,
-                height: 450.0,
+          Padding(
+            padding: const EdgeInsets.only(left: 300.0),
+            child: Wrap(
+              children: List.generate(
+                project.images?.length ?? 0,
+                (index) => Container(
+                  color: Colors.red,
+                  width: 200.0,
+                  height: 450.0,
+                ),
               ),
-              separator: 16.0,
+              spacing: 16.0,
+              runSpacing: 8.0,
             ),
           ),
           Column(
@@ -140,6 +112,7 @@ class ProjectContentLarge extends StatelessWidget {
                     .subtitle2
                     ?.copyWith(color: AppColors.textSecondary),
               ),
+              SizedBox(height: 16.0),
               if (project.description != null)
                 ConstrainedBox(
                   constraints: BoxConstraints(maxWidth: 500.0),
@@ -155,6 +128,7 @@ class ProjectContentLarge extends StatelessWidget {
                     ),
                   ),
                 ),
+              SizedBox(height: 8.0),
               ProjectLinksTags(project: project),
             ],
           ),
